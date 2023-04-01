e8dd67abf357939e01e82f712e660f87b5d23af1
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-04-01 01:01:49 +0100

    Added who to follow

--- components/Input.tsx
+++ components/Input.tsx
@@ -21,6 +21,7 @@ const Input: React.FC<InputProps> = ({
             onChange={onChange}
             placeholder={placeholder}
             type={type}
+            required
             value={value}
             className="
                 w-full
