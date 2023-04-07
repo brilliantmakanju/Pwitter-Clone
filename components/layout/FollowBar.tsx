c835670bb301eb8df0668cb1ba3ac9dec12a2495
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-04-07 01:38:25 +0100

    Excluded logged in user from follow bar

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -3,10 +3,12 @@ import Avatar from "../Avatar";
 
 const FollowBar = () => {
   const { data: users = [] } = useUsers();
-  
 
- 
- return (
+  if (users.length == 0) {
+    return null;
+  }
+
+  return (
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4  ">
         <h2 className="text-white text-xl font-semibold">Who to follow</h2>
